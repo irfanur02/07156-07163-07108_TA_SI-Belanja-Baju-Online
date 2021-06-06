406fae377efd66805f166d7b949516c39a7b3b9b
irfanur02 <irfanur820@gmail.com>
2021-06-07 00:24:03 +0700

    merubah semua file View dari .html menjadi .php

--- View/index.php
+++ View/index.php
@@ -7,8 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <link rel="stylesheet" href="../assets/font-awesome-4.7.0/css/font-awesome.css">
     <title>Belanja Baju Online</title>
@@ -16,50 +15,47 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-        <a class="navbar-brand mb-0 h1" href="../View/index.html">Belanja Baju Online</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand mb-0 h1" href="index.php?page=utama&aksi=view">Belanja Baju Online</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <form action="" method="" class="form-inline">
+                    <form action="index.php?page=utama&aksi=cariBaju" method="" class="form-inline">
                         <input class="form-control mr-sm-2" type="search" placeholder="Cari Baju" aria-label="Search">
                         <button class="btn btn-light my-2 my-sm-0 font-weight-bold" type="submit">Cari</button>
                     </form>
                 </li>
                 <li class="nav-item">
-                    <button type="button" class="btn btn-dark ml-2" data-toggle="modal"
-                        data-target="#modalFilterPencarianProduk">Filter</button>
+                    <button type="button" class="btn btn-dark ml-2" data-toggle="modal" data-target="#modalFilterPencarianProduk">Filter</button>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <div class="dropdown">
-                    <button class="btn btn-light dropdown-toggle mr-3 my-2 my-sm-0" id="dropdownMenuButton"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button class="btn btn-light dropdown-toggle mr-3 my-2 my-sm-0" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Nama
                     </button>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="../View/profil.html">Profil</a>
-                        <a class="dropdown-item" href="../View/favorite.html">Favorite</a>
-                        <a class="dropdown-item" href="../View/histori_transaksi.html">Histori Transaksi</a>
-                        <a class="dropdown-item" href="../View/index.html">Keluar</a>
+                        <a class="dropdown-item" href="index.php?page=profil&aksi=view">Profil</a>
+                        <a class="dropdown-item" href="index.php?page=favorite&aksi=view">Favorite</a>
+                        <a class="dropdown-item" href="index.php?page=historiTransaksi&aksi=view">Histori
+                            Transaksi</a>
+                        <a class="dropdown-item" href="index.php?page=utama&aksi=prosesLogout">Keluar</a>
                     </div>
                 </div>
-                <a href="../View/keranjang.html" class="btn btn-info border-dark mr-3 my-2 my-sm-0">
+                <a href="index.php?page=keranjang&aksi=view" class="btn btn-info border-dark mr-3 my-2 my-sm-0">
                     Keranjang <span class="badge badge-light" id="jumlahKeranjang">2</span>
                 </a>
-                <a href="../View/pembelian.html" class="btn btn-dark mr-3 my-2 my-sm-0">
+                <a href="index.php?page=pembelian&aksi=view" class="btn btn-dark mr-3 my-2 my-sm-0">
                     Pembelian
                 </a>
                 </a>
-                <button type="button" class="btn btn-primary border-white mr-3 my-2 my-sm-0" data-toggle="modal"
-                    data-target="#modalLogin">
+                <button type="button" class="btn btn-primary border-white mr-3 my-2 my-sm-0" data-toggle="modal" data-target="#modalLogin">
                     Login
                 </button>
-                <a href="../View/daftar.html" class="btn btn-light mr-3 my-2 my-sm-0">
+                <a href="index.php?page=daftar&aksi=view" class="btn btn-light mr-3 my-2 my-sm-0">
                     Daftar
                 </a>
             </form>
@@ -85,8 +81,7 @@
                             <div class="card-body">
                                 <div class="row justify-content-center">
                                     <div class="col-3">
-                                        <img src="../assets/img/casual 6.jpg" class="rounded img-fluid"
-                                            alt="Image Preview">
+                                        <img src="../assets/img/casual 6.jpg" class="rounded img-fluid" alt="Image Preview">
                                     </div>
                                     <div class="col-7">
                                         <div class="card" style="height: 100%;">
@@ -99,12 +94,10 @@
                                                 <blockquote class="blockquote font-weight-bold">
                                                     <p class="mb-0">Rp. 100.000</p>
                                                 </blockquote>
-                                                <a href="javascript:0" data-id="1"
-                                                    class="btn btn-primary tambahKeranjang">
+                                                <a href="javascript:0" data-id="1" class="btn btn-primary tambahKeranjang">
                                                     Masukkan Keranjang
                                                 </a>
-                                                <a href="javascript:0" data-id="1"
-                                                    class="btn btn-light border-dark simpanBaju">
+                                                <a href="javascript:0" data-id="1" class="btn btn-light border-dark simpanBaju">
                                                     <i class="fa fa-heart" aria-hidden="true"></i> Simpan
                                                 </a>
                                             </div>
@@ -119,8 +112,7 @@
                             <div class="card-body">
                                 <div class="row justify-content-center">
                                     <div class="col-3">
-                                        <img src="../assets/img/casual 6.jpg" class="rounded img-fluid"
-                                            alt="Image Preview">
+                                        <img src="../assets/img/casual 6.jpg" class="rounded img-fluid" alt="Image Preview">
                                     </div>
                                     <div class="col-7">
                                         <div class="card" style="height: 100%;">
@@ -133,12 +125,10 @@
                                                 <blockquote class="blockquote font-weight-bold">
                                                     <p class="mb-0">Rp. 100.000</p>
                                                 </blockquote>
-                                                <a href="javascript:0" data-id="2"
-                                                    class="btn btn-primary tambahKeranjang">
+                                                <a href="javascript:0" data-id="2" class="btn btn-primary tambahKeranjang">
                                                     Masukkan Keranjang
                                                 </a>
-                                                <a href="javascript:0" data-id="2"
-                                                    class="btn btn-light border-dark simpanBaju">
+                                                <a href="javascript:0" data-id="2" class="btn btn-light border-dark simpanBaju">
                                                     <i class="fa fa-heart" aria-hidden="true"></i> Simpan
                                                 </a>
                                             </div>
@@ -195,8 +185,7 @@
     </div>
 
     <!-- Modal -->
-    <div class="modal fade" id="modalLogin" tabindex="-1" role="dialog" aria-labelledby="modalCenterTitleLogin"
-        aria-hidden="true">
+    <div class="modal fade" id="modalLogin" tabindex="-1" role="dialog" aria-labelledby="modalCenterTitleLogin" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -205,21 +194,19 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form action="" method="post">
+                <form action="index.php?page=utama&aksi=prosesLogin" method="post">
                     <div class="modal-body">
                         <div class="form-group">
                             <label for="Username">Masukkan Username</label>
-                            <input type="text" class="form-control" id="inputUsername" name="username"
-                                placeholder="Username">
+                            <input type="text" class="form-control" id="inputUsername" name="username" placeholder="Username">
                         </div>
                         <div class="form-group">
                             <label for="Password">Masukkan Password</label>
-                            <input type="text" class="form-control" id="inputPassword" name="password"
-                                placeholder="Password">
+                            <input type="text" class="form-control" id="inputPassword" name="password" placeholder="Password">
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <a href="../View/index.html" class="btn btn-primary">Masuk</a>
+                        <button type="submit" class="btn btn-primary">Masuk</button>
                     </div>
                 </form>
             </div>
@@ -227,8 +214,7 @@
     </div>
 
     <!-- modal filter pencarian -->
-    <div class="modal fade bd-example-modal-lg" id="modalFilterPencarianProduk" tabindex="-1" role="dialog"
-        aria-labelledby="modalLabelFilterPencarianProduk" aria-hidden="true">
+    <div class="modal fade bd-example-modal-lg" id="modalFilterPencarianProduk" tabindex="-1" role="dialog" aria-labelledby="modalLabelFilterPencarianProduk" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -237,7 +223,7 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form action="" method="post">
+                <form action="index.php?page=utama&aksi=filterPencarian" method="post">
                     <div class="modal-body">
                         <span class="font-weight-bold">Berdasarkan :</span>
                         <div class="form-group row mt-2">
@@ -272,17 +258,13 @@
                                         <div class="col-sm-6 my-1 text-center">
                                             <div class="form-group">
                                                 <label for="formGroupExampleInput">Terkecil</label>
-                                                <input type="text" class="form-control" name="hargaTerkecil"
-                                                    value="100000" id="formGroupExampleInput"
-                                                    placeholder="Harga Terkecil">
+                                                <input type="text" class="form-control" name="hargaTerkecil" value="100000" id="formGroupExampleInput" placeholder="Harga Terkecil">
                                             </div>
                                         </div>
                                         <div class="col-sm-6 my-1 text-center">
                                             <div class="form-group">
                                                 <label for="formGroupExampleInput">Terbesar</label>
-                                                <input type="text" class="form-control" name="hargaTerbesar"
-                                                    value="1000000" id="formGroupExampleInput"
-                                                    placeholder="Harga Terbesar">
+                                                <input type="text" class="form-control" name="hargaTerbesar" value="1000000" id="formGroupExampleInput" placeholder="Harga Terbesar">
                                             </div>
                                         </div>
                                     </div>
@@ -292,15 +274,13 @@
                         <div class="row">
                             <div class="col">
                                 <div class="custom-control custom-checkbox float-right">
-                                    <input type="checkbox" class="custom-control-input" name="terlaris"
-                                        id="inputTerlaris">
+                                    <input type="checkbox" class="custom-control-input" name="terlaris" id="inputTerlaris">
                                     <label class="custom-control-label" for="inputTerlaris">Terpopuler</label>
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="disukai"
-                                        id="inputDisukai">
+                                    <input type="checkbox" class="custom-control-input" name="disukai" id="inputDisukai">
                                     <label class="custom-control-label" for="inputDisukai">Disukai</label>
                                 </div>
                             </div>
@@ -318,14 +298,11 @@
     <script src="../assets/js/jquery-3.6.0.min.js"></script>
     <script src="../assets/js/script.js"></script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 </body>
 
